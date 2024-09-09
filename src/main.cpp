a3f1a1c575d998d7b5322e7a507f363055b19476
Alexander Nähring <alexander@naehring.cloud>
2024-09-09 21:52:43 +0200

    only check RFID chip when BTN_CARD_INSIDE pin is LOW

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,11 @@
 #include "MFRC522DriverSPI.h"
 #include "MFRC522DriverPinSimple.h"
 #include "MFRC522Debug.h"
+#include "MFRC522Constants.h"
+
+
+// Button inputs
+#define BTN_CARD_INSIDE 17
 
 // SPI
 SPIClass spi_1(VSPI);
@@ -86,6 +91,7 @@ void setup() {
   Serial.begin(115200);
   Serial.println("Setup...");
 
+  pinMode(BTN_CARD_INSIDE, INPUT_PULLUP);
   
   Serial.println("SPI...");
   spi_1.begin(SD_SCK, SD_MISO, SD_MOSI, SD_CS);
@@ -120,11 +126,14 @@ void setup() {
 
 bool play_flag = false;
 
+unsigned int card_inside;
 unsigned long last_rfid_time;
+unsigned long last_rfid_check_time;
 MFRC522::Uid last_uid;
 unsigned long rfid_flag = 0;
 #define RFID_INTERVAL 400
 #define RFID_PAUSE_AFTER_INTERVAL 3
+#define RFID_CHECK_INTERVAL 50
 
 byte bufferATQA[2];
 byte bufferSize = sizeof(bufferATQA);
@@ -132,18 +141,18 @@ byte bufferSize = sizeof(bufferATQA);
 void loop() {
   unsigned long now = millis();
 
-  if(play_flag) {
+  if (play_flag) {
     if (mp3->isRunning()) {
-      unsigned long t = millis();
+      // unsigned long t = millis();
       if(!mp3->loop()) {
         Serial.println("mp3->stop();");
         mp3->stop();
       }
 
-      t = millis() - t;
-      Serial.print("mp3 loop took ");
-      Serial.print(t);
-      Serial.println("ms");
+      // t = millis() - t;
+      // Serial.print("mp3 loop took ");
+      // Serial.print(t);
+      // Serial.println("ms");
     } else {
       //source_sd->close();
       //source_sd->open("/music/test.mp3");
@@ -151,69 +160,53 @@ void loop() {
     }
   }
 
+  if (now - last_rfid_check_time > RFID_CHECK_INTERVAL) {
+    last_rfid_check_time = now;
+    if (digitalRead(BTN_CARD_INSIDE) == LOW) {
+      // card inside
+      if(!play_flag) {
+        // nothing is playing
+        // try to read card
+        // unsigned long t = millis();
+        Serial.print(".");
+        MFRC522::StatusCode result = mfrc522.PICC_RequestA(bufferATQA, &bufferSize);
+        // t = millis() - t;
+        // Serial.print("RFID check took ");
+        // Serial.print(t);
+        // Serial.println("ms");
+        
+        if (result == MFRC522::StatusCode::STATUS_OK) {
+          // RFID detected
+          Serial.println("RFID detected");
+          if (mfrc522.PICC_ReadCardSerial()) {
+            Serial.println(getHexString(mfrc522.uid.uidByte, mfrc522.uid.size));
+            // check if new RFID tag is equal to last_uid, or it's completely new -> resume or start fresh
+            if (compareUid(mfrc522.uid, last_uid)) {
+              // current RFID is same as last RFID, resume playback 
+              Serial.println("Resume playback");
+              play_flag = true;
+            } else {
+              // different RFID, read card and start new playback
+              memcpy(&last_uid, &mfrc522.uid, sizeof(mfrc522.uid));
 
-  if(now - last_rfid_time > RFID_INTERVAL) {
-    last_rfid_time = now;
+              Serial.println("Start new playback");
+              play_flag = true;
+              re_init_audio_source();
 
-    //if ( mfrc522.PICC_IsNewCardPresent()) {
-    unsigned long t = millis();
-    MFRC522::StatusCode result = mfrc522.PICC_RequestA(bufferATQA, &bufferSize);
-    t = millis() - t;
-    Serial.print("RFID check took ");
-    Serial.print(t);
-    Serial.println("ms");
-    if (result == MFRC522::StatusCode::STATUS_OK) {
-      // RFID detected
-      // Serial.println("RFID detected");
-      if (rfid_flag)  {
-        // if the RFID pause countdown is set, just re-set to maximum delay
-        rfid_flag = RFID_PAUSE_AFTER_INTERVAL;
-      } else {
-        // no current RFID pause countdown active -> start new or resume last playback
-        if (mfrc522.PICC_ReadCardSerial()) {
-          rfid_flag = RFID_PAUSE_AFTER_INTERVAL;
-          Serial.println(getHexString(mfrc522.uid.uidByte, mfrc522.uid.size));
-          // check if new RFID tag is equal to last_uid, or it's completely new -> resume or start fresh
-          if (compareUid(mfrc522.uid, last_uid)) {
-            // current RFID is same as last RFID, resume playback 
-            Serial.println("Resume playback");
-            play_flag = true;
-          } else {
-            // different RFID, read card and start new playback
-            memcpy(&last_uid, &mfrc522.uid, sizeof(mfrc522.uid));
-
-            Serial.println("Start new playback");
-            play_flag = true;
-
-            // if(mp3->isRunning())
-            //   mp3->stop();
-
-            // if (source_id3->isOpen()) {
-            //   Serial.println("Close last file");
-            //   source_id3->close();
-            // }
-            re_init_audio_source();
-
-            // source_sd->open("/music/Sonya Belousova - My Sails Are Set (Feat AURORA).mp3");
-            source_sd->open("/music/Alan Walker - Fade.mp3");
-            mp3->begin(source_id3, out_i2s);
-
-            // jumpstart playback, e.g. to fill source buffer
-            for (short int i = 0; i < 1000; i++) {
-              // if(mp3->isRunning())
-                mp3->loop();
+              // source_sd->open("/music/Sonya Belousova - My Sails Are Set (Feat AURORA).mp3");
+              source_sd->open("/music/Alan Walker - Fade.mp3");
+              mp3->begin(source_id3, out_i2s);
+              Serial.println("Go back to main loop...");
+              return;
             }
-            Serial.println("Go back to main loop...");
-            
+            // MFRC522Debug::PICC_DumpToSerial(mfrc522, Serial, &(mfrc522.uid));
           }
-          // MFRC522Debug::PICC_DumpToSerial(mfrc522, Serial, &(mfrc522.uid));
         }
+
       }
-    } else if(rfid_flag > 0) {
-      // no RFID detected and pause countdown is still active
-      rfid_flag -= 1;
-      if(rfid_flag == 0) {
-        Serial.println("RFID removed");
+    } else {
+      // no card inside
+      if (play_flag) {
         Serial.println("Pause playback");
         play_flag = false;
       }
