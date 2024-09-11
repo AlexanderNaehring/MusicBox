b5e2bb95b4096c2582c78e0ee63efac640de2429
Alexander Nähring <alexander@naehring.cloud>
2024-09-11 21:20:30 +0200

    Apply Google style guide format

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,17 @@
 #include <Arduino.h>
-#include <SPI.h>
 #include <SD.h>
+#include <SPI.h>
 
-#include "AudioFileSourceSD.h"
 #include "AudioFileSourceBuffer.h"
 #include "AudioFileSourceID3.h"
-#include "AudioOutputI2S.h"
+#include "AudioFileSourceSD.h"
 #include "AudioGeneratorMP3.h"
-
-#include "MFRC522v2.h"
-#include "MFRC522DriverSPI.h"
-#include "MFRC522DriverPinSimple.h"
-#include "MFRC522Debug.h"
+#include "AudioOutputI2S.h"
 #include "MFRC522Constants.h"
-
+#include "MFRC522Debug.h"
+#include "MFRC522DriverPinSimple.h"
+#include "MFRC522DriverSPI.h"
+#include "MFRC522v2.h"
 
 // Button inputs
 #define BTN_CARD_INSIDE 17
@@ -34,7 +32,8 @@ SPIClass spi_2(HSPI);
 #define RFID_SCK 14
 #define RFID_CS 15
 MFRC522DriverPinSimple ss_pin(RFID_CS);
-const SPISettings spiSettings = SPISettings(SPI_CLOCK_DIV4, MSBFIRST, SPI_MODE0); 
+const SPISettings spiSettings =
+    SPISettings(SPI_CLOCK_DIV4, MSBFIRST, SPI_MODE0);
 MFRC522DriverSPI spi_driver{ss_pin, spi_2, spiSettings};
 MFRC522 mfrc522{spi_driver};
 
@@ -49,24 +48,21 @@ AudioOutputI2S *out_i2s = NULL;
 String getHexString(byte *buffer, byte bufferSize) {
   String id = "";
   for (byte i = 0; i < bufferSize; i++) {
-  id += buffer[i] < 0x10 ? "0" : "";
-  id += String(buffer[i], HEX);
+    id += buffer[i] < 0x10 ? "0" : "";
+    id += String(buffer[i], HEX);
   }
   return id;
 }
 
 bool compareUid(MFRC522::Uid &uid1, MFRC522::Uid &uid2) {
-  if (uid1.size != uid2.size)
-    return false;
+  if (uid1.size != uid2.size) return false;
   for (byte i = 0; i < uid1.size; i++) {
-    if (uid1.uidByte[i] != uid2.uidByte[i])
-      return false;
+    if (uid1.uidByte[i] != uid2.uidByte[i]) return false;
   }
   return true;
 }
 
 void re_init_audio_source() {
-
   if (source_sd) {
     delete source_sd;
     source_sd = NULL;
@@ -81,9 +77,12 @@ void re_init_audio_source() {
   }
 
   source_sd = new AudioFileSourceSD();
-  // uint8_t *buffer = (uint8_t*)malloc(sizeof(uint8_t) * AUDIO_SOURCE_BUFFER_SIZE);
-  source_buffer = new AudioFileSourceBuffer(source_sd, AUDIO_SOURCE_BUFFER_SIZE);
-  // ID3 file source for MP3 files reduces the delay until playback starts, and enabled ID3 Tag callbacks
+  // uint8_t *buffer = (uint8_t*)malloc(sizeof(uint8_t) *
+  // AUDIO_SOURCE_BUFFER_SIZE);
+  source_buffer =
+      new AudioFileSourceBuffer(source_sd, AUDIO_SOURCE_BUFFER_SIZE);
+  // ID3 file source for MP3 files reduces the delay until playback starts, and
+  // enabled ID3 Tag callbacks
   source_id3 = new AudioFileSourceID3(source_buffer);
 }
 
@@ -92,32 +91,36 @@ void setup() {
   Serial.println("Setup...");
 
   pinMode(BTN_CARD_INSIDE, INPUT_PULLUP);
-  
+
   Serial.println("SPI...");
   spi_1.begin(SD_SCK, SD_MISO, SD_MOSI, SD_CS);
   Serial.println("SD...");
-  if(!SD.begin(SD_CS, spi_1)) {
+  if (!SD.begin(SD_CS, spi_1)) {
     Serial.println("Card Mount Failed");
-    while(1) {}
+    while (1) {
+    }
   }
-
 
   Serial.println("RFID...");
   mfrc522.PCD_Init();
   MFRC522Debug::PCD_DumpVersionToSerial(mfrc522, Serial);
 
-  //file = SD.open("/music/Sonya Belousova - My Sails Are Set (Feat AURORA).mp3");
+  // file = SD.open("/music/Sonya Belousova - My Sails Are Set (Feat
+  // AURORA).mp3");
 
   audioLogger = &Serial;
   Serial.println("Audio...");
   source_sd = new AudioFileSourceSD();
-  //uint8_t *buffer = (uint8_t*)malloc(sizeof(uint8_t) * AUDIO_SOURCE_BUFFER_SIZE);
-  source_buffer = new AudioFileSourceBuffer(source_sd, AUDIO_SOURCE_BUFFER_SIZE);
-  // ID3 file source for MP3 files reduces the delay until playback starts, and enabled ID3 Tag callbacks
+  // uint8_t *buffer = (uint8_t*)malloc(sizeof(uint8_t) *
+  // AUDIO_SOURCE_BUFFER_SIZE);
+  source_buffer =
+      new AudioFileSourceBuffer(source_sd, AUDIO_SOURCE_BUFFER_SIZE);
+  // ID3 file source for MP3 files reduces the delay until playback starts, and
+  // enabled ID3 Tag callbacks
   source_id3 = new AudioFileSourceID3(source_buffer);
 
   out_i2s = new AudioOutputI2S();
-  out_i2s->SetGain(10/100.0);
+  out_i2s->SetGain(10 / 100.0);
 
   mp3 = new AudioGeneratorMP3();
 
@@ -144,7 +147,7 @@ void loop() {
   if (play_flag) {
     if (mp3->isRunning()) {
       // unsigned long t = millis();
-      if(!mp3->loop()) {
+      if (!mp3->loop()) {
         Serial.println("mp3->stop();");
         mp3->stop();
       }
@@ -154,9 +157,9 @@ void loop() {
       // Serial.print(t);
       // Serial.println("ms");
     } else {
-      //source_sd->close();
-      //source_sd->open("/music/test.mp3");
-      //mp3->begin(source_id3, out_i2s);
+      // source_sd->close();
+      // source_sd->open("/music/test.mp3");
+      // mp3->begin(source_id3, out_i2s);
     }
   }
 
@@ -164,25 +167,27 @@ void loop() {
     last_rfid_check_time = now;
     if (digitalRead(BTN_CARD_INSIDE) == LOW) {
       // card inside
-      if(!play_flag) {
+      if (!play_flag) {
         // nothing is playing
         // try to read card
         // unsigned long t = millis();
         Serial.print(".");
-        MFRC522::StatusCode result = mfrc522.PICC_RequestA(bufferATQA, &bufferSize);
+        MFRC522::StatusCode result =
+            mfrc522.PICC_RequestA(bufferATQA, &bufferSize);
         // t = millis() - t;
         // Serial.print("RFID check took ");
         // Serial.print(t);
         // Serial.println("ms");
-        
+
         if (result == MFRC522::StatusCode::STATUS_OK) {
           // RFID detected
           Serial.println("RFID detected");
           if (mfrc522.PICC_ReadCardSerial()) {
             Serial.println(getHexString(mfrc522.uid.uidByte, mfrc522.uid.size));
-            // check if new RFID tag is equal to last_uid, or it's completely new -> resume or start fresh
+            // check if new RFID tag is equal to last_uid, or it's completely
+            // new -> resume or start fresh
             if (compareUid(mfrc522.uid, last_uid)) {
-              // current RFID is same as last RFID, resume playback 
+              // current RFID is same as last RFID, resume playback
               Serial.println("Resume playback");
               play_flag = true;
             } else {
@@ -193,7 +198,8 @@ void loop() {
               play_flag = true;
               re_init_audio_source();
 
-              // source_sd->open("/music/Sonya Belousova - My Sails Are Set (Feat AURORA).mp3");
+              // source_sd->open("/music/Sonya Belousova - My Sails Are Set
+              // (Feat AURORA).mp3");
               source_sd->open("/music/Alan Walker - Fade.mp3");
               mp3->begin(source_id3, out_i2s);
               Serial.println("Go back to main loop...");
@@ -202,7 +208,6 @@ void loop() {
             // MFRC522Debug::PICC_DumpToSerial(mfrc522, Serial, &(mfrc522.uid));
           }
         }
-
       }
     } else {
       // no card inside
